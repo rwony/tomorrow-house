e7f2a03e37da547cb1a4bbe8e06a59ffe462c403
rwony <dev.rwlee@gmail.com>
2022-10-31 22:34:59 +0900

    💫 Scroll to tab-panel when product-tab item is clicked

--- js/product-tab.js
+++ js/product-tab.js
@@ -1,8 +1,15 @@
 const productTab = document.querySelector('.product-tab')
 const productTabButtonList = productTab.querySelectorAll('button')
 
+const TOP_HEADER_DESKTOP = 80 + 50 + 54
+const TOP_HEADER_MOBILE = 50 + 40 + 40
+
 let currentActiveTab = productTab.querySelector('.is-active')
 
+/*
+ * name: toggleActiveTab()
+ * desc: 탭 클릭 시 해당 탭에 is-active 클래스를 추가한다.
+ */
 function toggleActiveTab() {
   const tabItem = this.parentNode
 
@@ -13,6 +20,21 @@ function toggleActiveTab() {
   }
 }
 
+/*
+ * name: scrollTabPanel()
+ * desc: 탭 클릭 시 해당 탭의 위치로 스크롤이 이동한다.
+ */
+function scrollTabPanel() {
+  const tabPanelId = this.parentNode.getAttribute('aria-labelledby')
+  const tabPanel = document.querySelector(`#${tabPanelId}`)
+  const scrollAmount =
+    tabPanel.getBoundingClientRect().top -
+    (window.innerWidth >= 768 ? TOP_HEADER_DESKTOP : TOP_HEADER_MOBILE)
+
+  window.scrollBy({ top: scrollAmount, behavior: 'smooth' })
+}
+
 productTabButtonList.forEach((button) => {
   button.addEventListener('click', toggleActiveTab)
+  button.addEventListener('click', scrollTabPanel)
 })
