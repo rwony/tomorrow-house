02272b78fd85262c8ae62b685d8768c22f94d07d
rwony <dev.rwlee@gmail.com>
2022-11-06 12:06:50 +0900

    🚧 Change bookmark-toast style and Add background-dark color

--- js/order-cta.js
+++ js/order-cta.js
@@ -1,6 +1,13 @@
 const orderCta = document.querySelector('.order-cta')
 const [orderCtaBookmarkButton, orderCtaBuyButton] = orderCta.children
 
+let bookmarkToastList = document.querySelectorAll('.bookmark-toast')
+const bookmarkToast = bookmarkToastList[0]
+const bookmarkToastRemove = bookmarkToastList[1]
+const bookmarkToastCloseButtonList = document.querySelectorAll(
+  '.bookmark-toast .close-button'
+)
+
 const orderModal = document.querySelector('.order-form-modal')
 const orderModalOverlay = document.querySelector('.overlay')
 
@@ -30,8 +37,39 @@ function checkBookmark() {
   this.classList.toggle('is-active')
   countSpan.setAttribute('aria-label', `북마크 ${newCount.toLocaleString()}회`)
   countSpan.textContent = newCount.toLocaleString()
+
+  showBookmarkToast()
+}
+
+/*
+ * name: showBookmarkToast()
+ * desc: 북마크 여부에 따른 스크랩 모달을 보여준다.
+ */
+function showBookmarkToast() {
+  if (orderCtaBookmarkButton.classList.contains('is-active')) {
+    bookmarkToast.classList.add('is-active')
+    bookmarkToastRemove.classList.remove('is-active')
+  } else {
+    bookmarkToast.classList.remove('is-active')
+    bookmarkToastRemove.classList.add('is-active')
+  }
+
+  setTimeout(() => {
+    bookmarkToastList.forEach((item) => {
+      if (item.classList.contains('is-active')) {
+        item.classList.remove('is-active')
+      }
+    })
+  }, 3500)
+}
+
+function closeBookmarkToast() {
+  this.parentNode.classList.remove('is-active')
 }
 
 orderCtaBuyButton.addEventListener('click', openOrderModal)
 orderModalOverlay.addEventListener('click', closeOrderModal)
 orderCtaBookmarkButton.addEventListener('click', checkBookmark)
+bookmarkToastCloseButtonList.forEach((button) => {
+  button.addEventListener('click', closeBookmarkToast)
+})
