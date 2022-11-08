53d831ac6f79514699a64cd937d02ad5d4720ef0
rwony <dev.rwlee@gmail.com>
2022-11-08 15:59:32 +0900

    🚧 Implement total amount according to changed select box

--- js/checkout.js
+++ js/checkout.js
@@ -6,7 +6,7 @@ const checkoutItemLi = checkoutItemList[0].querySelector('.checkout-item')
 
 const checkoutItems = document.querySelectorAll('.checkout-item')
 const checkoutFooter = document.querySelectorAll('.checkout-footer')
-const orderAmount = document.querySelector('.order-summary')
+const orderAmount = document.querySelectorAll('.order-summary')
 
 function addCheckoutItem() {
   const selectedItem = this.options[this.selectedIndex]
@@ -18,24 +18,46 @@ function addCheckoutItem() {
     templete.setAttribute('data-price', price)
     templete.querySelector('h4').innerHTML = name
     templete.querySelector('.amount').innerHTML = price.toLocaleString()
+    templete.querySelector('select').addEventListener('change', changeAmount)
     it.append(templete)
   })
+
+  getTotalAmount()
 }
 
 function changeAmount() {
   const originalPrice = Number(this.closest('.checkout-item').dataset.price)
   const amountElement =
     this.parentNode.nextElementSibling.querySelector('.amount')
+  console.log(this.options[this.selectedIndex])
   const count = Number(this.options[this.selectedIndex].text)
 
   const amount = (originalPrice * count).toLocaleString()
   amountElement.innerHTML = amount
+
+  getTotalAmount()
+}
+
+function getTotalAmount() {
+  const checkoutLiItems = checkoutItemList[0].children
+  let amount = 0
+
+  HTMLCollection.prototype.forEach = Array.prototype.forEach
+  checkoutLiItems.forEach((element) => {
+    amount += Number(
+      element.querySelector('.amount').innerHTML.replaceAll(',', '')
+    )
+  })
+
+  orderAmount.forEach((element) => {
+    element.querySelector('.amount').innerHTML = amount.toLocaleString()
+  })
 }
 
 orderSelect.forEach((item) => {
   item.addEventListener('change', addCheckoutItem)
 })
 
-checkoutItems.forEach((item) => {
+checkoutItemList.forEach((item) => {
   item.querySelector('select').addEventListener('change', changeAmount)
 })
